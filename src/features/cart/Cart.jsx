81d54a8f95dfd4b5dfea2cad0fb3449a67c39dfb
Mira <kruzinka@yandex.ru>
2024-08-19 23:38:05 +0300

    making a loader

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Cart() {
-    return (
-      <div>
-        <h1>CART</h1>
-      </div>
-    );
-  }
-  
-  export default Cart;
+  return (
+    <div>
+      <Link to="/menu">Back to manu</Link>
+      <h1>CART</h1>
+    </div>
+  );
+}
+
+export default Cart;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,17 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const nav = useNavigation();
+  const isLoading = nav.state === "loading";
+
+  // console.log(nav);
+
   return (
     <div>
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
