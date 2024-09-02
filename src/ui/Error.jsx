1fbd169715f75686135c19e3b451d65c6df8fc13
Mira <kruzinka@yandex.ru>
2024-09-02 22:35:12 +0300

    error handling

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,7 +1,14 @@
+import { useNavigate, useRouteError } from "react-router-dom";
+
 function Error() {
+  const navigate = useNavigate();
+  const error = useRouteError();
+
   return (
     <div>
-      <h1>ERROR</h1>
+      <h1>Oh no!</h1>
+      <p>{error.data || error.message}</p>
+      <button onClick={() => navigate(-1)}>GO BACK</button>
     </div>
   );
 }
