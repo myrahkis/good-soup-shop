735ad0c50982a77f67da6ea7639f6d64ba33a5de
Mira <kruzinka@yandex.ru>
2024-09-06 23:49:24 +0300

    setting up tailwind

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,9 +1,13 @@
 function Home() {
-    return (
-      <div>
-        <h1>HOME</h1>
-      </div>
-    );
-  }
-  
-  export default Home;
+  return (
+    <div>
+      <h1 className="text-center text-pink-500 font-semibold">
+        The best soups.
+        <br />
+        Good soup, good for you.
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
