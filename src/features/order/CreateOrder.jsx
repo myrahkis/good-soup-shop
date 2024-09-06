c92caab7d9cdcca9cc21aed7e58a7596773fee6b
Mira <kruzinka@yandex.ru>
2024-09-06 16:21:17 +0300

    form validation (error handling)

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,10 @@
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigate } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+
+const isValidPhone = (str) =>
+  /^\+?\d{1,7}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
 
 const fakeCart = [
   {
@@ -19,6 +24,10 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigate();
+  const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
+
   const cart = fakeCart;
 
   return (
@@ -32,6 +41,7 @@ function CreateOrder() {
         <div>
           <label>Phone number</label>
           <input type="tel" name="phone" required />
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
         <div>
           <label>Address</label>
@@ -45,7 +55,9 @@ function CreateOrder() {
         </div>
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
@@ -63,12 +75,21 @@ export async function action({ request }) {
     data: {
       ...data,
       cart: JSON.parse(data.cart),
-      priority: data.priority === 'on',
+      priority: data.priority === "on",
     },
     name: data.customer,
-  }
+  };
+
+  const errors = {};
+
+  if (!isValidPhone(order.data.phone))
+    errors.phone =
+      "Please enter a valid phone number. We might need it to contact you.";
+
+  if (Object.keys(errors).length > 0) return errors;
 
   // console.log(order);
+  // if ok create nwe order and redirect
   const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);
